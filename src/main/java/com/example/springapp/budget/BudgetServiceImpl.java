0dc0bdd9139ad6d2b2f660d2f10f2f55749135b9
OanaBiancaTuca <tuca.oana.bianca30@gmail.com>
2024-05-10 16:24:33 +0300

    Initial commit

---
+++ src/main/java/com/example/springapp/budget/BudgetServiceImpl.java
@@ -0,0 +1,77 @@
+package com.example.springapp.budget;
+
+import com.example.springapp.category.Category;
+import com.example.springapp.category.CategoryService;
+import com.example.springapp.user.UserEntity;
+import com.example.springapp.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BudgetServiceImpl implements BudgetService {
+    @Autowired
+    private BudgetRepository budgetRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    /*public BudgetServiceImpl(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }*/
+
+    @Override
+    public List<Budget> getAllBudgetByUser(UserEntity user) {
+        return budgetRepository.findAllByUser(user.getUserId());
+    }
+
+    @Override
+    public Optional<Budget> getBudgetById(Long id) {
+        return budgetRepository.findById(id);
+    }
+
+    @Override
+    public Budget createBudget(BudgetRequestDto budgetRequestDto, String userName) {
+        Category category = categoryService.getCategoryById(budgetRequestDto.getCategoryId());
+        UserEntity user = userRepository.findByEmail(userName).orElseThrow();
+        Budget budget = new Budget(category, budgetRequestDto.getAmount(),user,0L,(long) budgetRequestDto.getAmount());
+        //modify by me
+        long balance = (long) budgetRequestDto.getAmount();
+        budget.setBalance(balance);
+        return budgetRepository.save(budget);
+    }
+
+
+    @Override
+    public Budget updateBudget(Budget budget) {
+        budget.setBalance((long) (budget.getAmount()-budget.getUsed()));
+        return budgetRepository.save(budget);
+    }
+
+    @Override
+    public void deleteBudget(Long id) {
+        budgetRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean hasAlready(String userName, int categoryId) {
+        UserEntity user = userRepository.findByEmail(userName).orElseThrow();
+        List<Budget> budgetList = budgetRepository.findAllByUser(user.getUserId());
+        boolean isAlready = false;
+        for (Budget b: budgetList
+             ) {
+            if (b.getCategory().getCategoryId() == categoryId) {
+                isAlready = true;
+                break;
+            }
+        }
+        return  isAlready;
+    }
+
+
+}
